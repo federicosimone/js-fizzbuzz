42c006430a0604bafc0b0e2c71dfd43f5b41a6d7
federicosimone <federico.simone15@gmail.com>
2025-10-29 22:46:09 +0100

    primi tentativi

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("js funziona");
+//ho bisogno di creare un programma che stampi i numeri da 1 a 100 e utilizzo un ciclo for
+
+
+for (let i=1 ; i<=100 ; i++) { 
+    if (i % 3 == 0) { 
+        console.log("FIZZ") ;    
+    } else if (i % 5 == 0) {
+        console.log("BUZZ") ;
+    } else if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FIZZBUZZ") ;
+    } else {
+        console.log(i)
+    }
+    
+}
+
+// nelle istruzioni gli devo dire che se è un multiplo di 3 allora deve stampare fizz
+// altrimenti se è un multiplo di 5 , allora deve stampare buzz
+// altrimenti se è un multiplo sia di 5 che di 3 . allora deve stampare fizzbuzz
