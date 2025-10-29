5c900e8cf4a537a86c24eda64986d838ddbbec5a
federicosimone <federico.simone15@gmail.com>
2025-10-29 23:24:13 +0100

    risoluzione problema

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-//ho bisogno di creare un programma che stampi i numeri da 1 a 100 e utilizzo un ciclo for
-
-
-
-/*for (let i=1 ; i<=100 ; i++) { 
-    
-    if (i % 3 == 0) { 
-        console.log("FIZZ") ;    
-    } else if (i % 5 == 0) {
-        console.log("BUZZ") ;
-    } else if (i % 3 == 0 && i % 5 == 0) {
-        console.log("FIZZBUZZ") ;
-    } else {
-        console.log(i) ;
-    }
-    
-}
-*/    
+//ho bisogno di creare un programma che stampi i numeri da 1 a 100 e utilizzo un ciclo for    
  for (let i=1 ; i<=100 ; i++) {
     if(i % 3 == 0 && i % 5 == 0) {
         console.log("FizzBuzz") ;
