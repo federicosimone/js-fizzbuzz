10f1d3c0fb1cd1a1d633f5a70c200c2643c15aed
federicosimone <federico.simone15@gmail.com>
2025-10-29 23:20:53 +0100

    modifica

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //ho bisogno di creare un programma che stampi i numeri da 1 a 100 e utilizzo un ciclo for
 
 
-for (let i=1 ; i<=100 ; i++) { 
+
+/*for (let i=1 ; i<=100 ; i++) { 
+    
     if (i % 3 == 0) { 
         console.log("FIZZ") ;    
     } else if (i % 5 == 0) {
@@ -9,10 +11,23 @@ for (let i=1 ; i<=100 ; i++) {
     } else if (i % 3 == 0 && i % 5 == 0) {
         console.log("FIZZBUZZ") ;
     } else {
-        console.log(i)
+        console.log(i) ;
     }
     
 }
+*/    
+ for (let i=1 ; i<=100 ; i++) {
+    if(i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz") ;
+    } else if (i % 3 == 0) {
+        console.log("Fizz") ;
+    } else if (i % 5 == 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i) ;
+    }
+ }
+
 
 // nelle istruzioni gli devo dire che se è un multiplo di 3 allora deve stampare fizz
 // altrimenti se è un multiplo di 5 , allora deve stampare buzz
